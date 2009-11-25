a4dda7f7a6ac01e445b98ae4825f561c4216c369
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2009-11-25 06:14:52 -0500

    Fully mimic sha1c.py

--- src/oclSha1/sha1.cpp
+++ src/oclSha1/sha1.cpp
@@ -1,26 +1,22 @@
 #include <oclUtils.h>
 
-void sha1(uint *W, uint *H, const uint *work_items);
-void prepare(char *msg, const int *len, uint *W);
-void hexdigest(const uint *H, char *digest);
-
 int main(int argc, char **argv)
 {
   cl_int ciErr;
 
   // Create Context
-  cl_context cxGPUContext = clCreateContextFromType(0, CL_DEVICE_TYPE_GPU, NULL, NULL, &ciErr);
+  cl_context cxGPUContext = clCreateContextFromType(0, CL_DEVICE_TYPE_GPU, 0, 0, &ciErr);
   oclCheckError(ciErr);
 
   // Get the list of GPU devices associated with context
   size_t szParmDataBytes;
 
-  ciErr = clGetContextInfo(cxGPUContext, CL_CONTEXT_DEVICES, 0, NULL, &szParmDataBytes);
+  ciErr = clGetContextInfo(cxGPUContext, CL_CONTEXT_DEVICES, 0, 0, &szParmDataBytes);
   oclCheckError(ciErr);
 
   cl_device_id* cdDevices = (cl_device_id*)malloc(szParmDataBytes);
 
-  ciErr = clGetContextInfo(cxGPUContext, CL_CONTEXT_DEVICES, szParmDataBytes, cdDevices, NULL);
+  ciErr = clGetContextInfo(cxGPUContext, CL_CONTEXT_DEVICES, szParmDataBytes, cdDevices, 0);
   oclCheckError(ciErr);
 
   // Create a command-queue
@@ -30,43 +26,73 @@ int main(int argc, char **argv)
   // Read the OpenCL kernel in from source file
   size_t szProgramLength;
 
-  char *cPathAndName = shrFindFilePath("sha1.cl", argv[0]);
-  shrCheckError(cPathAndName != NULL, shrTRUE);
+  char *cPathAndName = shrFindFilePath("sha1c.cl", argv[0]);
+  shrCheckError(cPathAndName != 0, shrTRUE);
 
   char *cSourceCL = oclLoadProgSource(cPathAndName, "", &szProgramLength);
-  shrCheckError(cSourceCL != NULL, shrTRUE);
+  shrCheckError(cSourceCL != 0, shrTRUE);
 
   cl_program cpProgram = clCreateProgramWithSource(cxGPUContext, 1, (const char **)&cSourceCL, &szProgramLength, &ciErr);
   oclCheckError(ciErr);
 
   // Build the program
-  ciErr = clBuildProgram(cpProgram, 0, NULL, NULL, NULL, NULL);
-  oclCheckError(ciErr);
-
-  // Create the "prepare" kernel
-  cl_kernel ckKernelPrepare = clCreateKernel(cpProgram, "prepare", &ciErr);
+  ciErr = clBuildProgram(cpProgram, 0, 0, 0, 0, 0);
   oclCheckError(ciErr);
 
   // Allocate the OpenCL buffer memory objects for source and result on the device GMEM
-  cl_mem cmDevSrcA = clCreateBuffer(cxGPUContext, CL_MEM_READ_ONLY, SIZE, NULL, &ciErr);
+  char *message = (char *)malloc(64);
+  unsigned int messageLength = 3;
+  unsigned char *digest = (unsigned char *)malloc(41);
+
+  strcpy(message, "abc");
+  memset(digest, 0, 41);
+
+  cl_mem cmMessage = clCreateBuffer(cxGPUContext, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, 64, message, &ciErr);
   oclCheckError(ciErr);
-  
-  cl_mem cmDevSrcB = clCreateBuffer(cxGPUContext, CL_MEM_READ_ONLY, SIZE, NULL, &ciErr);
+
+  cl_mem cmLength = clCreateBuffer(cxGPUContext, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, 4, &messageLength, &ciErr);
   oclCheckError(ciErr);
-  
-  cl_mem cmDevDst = clCreateBuffer(cxGPUContext, CL_MEM_WRITE_ONLY, SIZE, NULL, &ciErr);
+
+  cl_mem cmDigest = clCreateBuffer(cxGPUContext, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, 41, digest, &ciErr);
   oclCheckError(ciErr);
-  
+
+  // Create the "prepare" kernel
+  cl_kernel ckKernel = clCreateKernel(cpProgram, "sha1", &ciErr);
+  oclCheckError(ciErr);
+
   // Set the Argument values
-  ciErr = clSetKernelArg(ckKernel, 0, sizeof(cl_mem), (void*)&cmDevSrcA);
+  ciErr = clSetKernelArg(ckKernel, 0, sizeof(cl_mem), (void*)&cmMessage);
   oclCheckError(ciErr);
-  
-  ciErr = clSetKernelArg(ckKernel, 1, sizeof(cl_mem), (void*)&cmDevSrcB);
+
+  ciErr = clSetKernelArg(ckKernel, 1, sizeof(cl_mem), (void*)&cmLength);
   oclCheckError(ciErr);
-  
-  ciErr = clSetKernelArg(ckKernel, 2, sizeof(cl_mem), (void*)&cmDevDst);
+
+  ciErr = clSetKernelArg(ckKernel, 2, sizeof(cl_mem), (void*)&cmDigest);
   oclCheckError(ciErr);
-  
-  ciErr = clSetKernelArg(ckKernel, 3, sizeof(cl_int), (void*)&iNumElements);
+
+  // Launch kernel
+  size_t szGlobalWorkSize = 4;
+  ciErr = clEnqueueNDRangeKernel(cqCommandQueue, ckKernel, 1, 0, &szGlobalWorkSize, 0, 0, 0, 0);
   oclCheckError(ciErr);
+
+  // Synchronous/blocking read of results, and check accumulated errors
+  ciErr = clEnqueueReadBuffer(cqCommandQueue, cmDigest, CL_TRUE, 0, 40, digest, 0, 0, 0);
+  oclCheckError(ciErr);
+
+  fprintf(stderr, "digest: %s\n", digest);
+
+  if (cdDevices)      free(cdDevices);
+  if (cPathAndName)   free(cPathAndName);
+  if (cSourceCL)      free(cSourceCL);
+  if (ckKernel)       clReleaseKernel(ckKernel);
+  if (cpProgram)      clReleaseProgram(cpProgram);
+  if (cqCommandQueue) clReleaseCommandQueue(cqCommandQueue);
+  if (cxGPUContext)   clReleaseContext(cxGPUContext);
+  if (cmMessage)      clReleaseMemObject(cmMessage);
+  if (cmLength)       clReleaseMemObject(cmLength);
+  if (cmDigest)       clReleaseMemObject(cmDigest);
+  if (message)        free(message);
+  if (digest)         free(digest);
+
+  return 0;
 }
