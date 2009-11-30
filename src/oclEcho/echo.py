e1e4116564d3204a4d88f5ae41f8dd2d20c4eade
Gabriel Stocco <gabe@c221-5.kiewit.dartmouth.edu>
2009-11-30 04:40:13 -0500

    Adding echo datas. Modified echo.py for evaluation.

--- src/oclEcho/echo.py
+++ src/oclEcho/echo.py
@@ -4,8 +4,15 @@ import pyopencl as cl
 import numpy as np
 import sys
 
+interval=64
+maxi=8192
+runs=10
+datas=[]
+for i in range(0,runs,1):
+  datas.append([])
+
 src   = open('echo_new.cl', 'r').read()
-ctx   = cl.Context(dev_type=cl.device_type.GPU)
+ctx   = cl.Context(dev_type=cl.device_type.CPU)
 queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
 prg   = cl.Program(ctx, src).build()
 
@@ -31,23 +38,26 @@ databitlen_buf  = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST
 
 state_buf       = cl.Buffer(ctx, cl.mem_flags.READ_WRITE | cl.mem_flags.ALLOC_HOST_PTR, size=statesize)
 
-for i in range(0, 1024+32, 32):
-  events  = []
-  datalen = 0
+for k in range(0, runs,1):
+  for i in range(0, maxi+interval, interval):
+    events  = []
+    datalen = 0
 
-  events.append( prg.Init(queue, (1,), state_buf, hashbitlen_buf) )
+    events.append( prg.Init(queue, (1,), state_buf, hashbitlen_buf) )
 
-  for j in range(0, i, 32):
-    datalen += databitlen / 8
-    events.append( prg.Update(queue, (1,), state_buf, data_buf, databitlen_buf) )
+    for j in range(0, i, 32):
+      datalen += databitlen / 8
+      events.append( prg.Update(queue, (1,), state_buf, data_buf, databitlen_buf) )
 
-  events.append( prg.Final(queue, (1,), state_buf, hashval_buf) )
+    events.append( prg.Final(queue, (1,), state_buf, hashval_buf) )
 
-  events[-1].wait()
-  cl.enqueue_read_buffer(queue, hashval_buf, hashval).wait()
+    events[-1].wait()
+    cl.enqueue_read_buffer(queue, hashval_buf, hashval).wait()
 
-  hashval_hex = ""
-  for i in xrange(0, hashval.size):
-    hashval_hex += "%02x" % hashval[i]
+    datas[k].append(sum(evt.profile.end - evt.profile.start for evt in events))
 
-  print "%d\t%lu\t%s" % (datalen, sum(evt.profile.end - evt.profile.start for evt in events), hashval_hex)
+for i in range(0, (maxi+interval)/interval, 1):
+  time=0
+  for j in range(0, runs, 1):
+    time+=datas[j][i]
+  print "%d\t%lu" % (i*interval, time/runs)
